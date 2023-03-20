108ae6ee5285c00ed2064633b3ad2884a97098ea
Nick Milburn <nmilbu1@wgu.edu>
2023-03-19 17:59:03 -0700

    fixed lint error

--- client/src/_helpers/bundler/unpkg-path-plugin.ts
+++ client/src/_helpers/bundler/unpkg-path-plugin.ts
@@ -1,26 +1,23 @@
-import * as esbuild from 'esbuild-wasm';
+import * as esbuild from "esbuild-wasm";
 
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /(^index\.js$)/ }, () => {
-        return {path: "index.js", namespace: "a" };
+        return { path: "index.js", namespace: "a" };
       });
 
       build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
-          namespace: 'a',
-          path: new URL(
-            args.path,
-            'https://unpkg.com' + args.resolveDir + '/'
-          ).href,
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/").href,
         };
       });
 
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
-          namespace: 'a',
+          namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
       });
