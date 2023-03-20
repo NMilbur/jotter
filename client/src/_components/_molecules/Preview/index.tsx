c8c3f580eb330d6741ed316748f38a50bd4227bf
Nick Milburn <nmilbu1@wgu.edu>
2023-03-19 17:43:23 -0700

    refactored and reorged to fit atoms, molecules, organisms structure for components

---
+++ client/src/_components/_molecules/Preview/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+
+const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener("message", (event) => {
+          try {
+            eval(event.data);
+          } catch(err) {
+            const root = document.querySelector("#root");
+            root.innerHTML = "<div style='color: red;'><h4>Runtime error</h4>" + err + "</div>";
+            throw err;
+          }
+        }, false);
+      </script>
+    </body>
+  </html>
+`;
+
+interface PreviewProps {
+  code: string;
+}
+
+const Preview = ({ code }: PreviewProps) => {
+  const iframeRef = useRef<any>();
+
+  useEffect(() => {
+    iframeRef.current.srcdoc = html;
+    iframeRef.current.contentWindow.postMessage(code, "*");
+  }, [code]);
+
+  return <iframe title="preview" ref={iframeRef} sandbox="allow-scripts" srcDoc={html} />;
+};
+
+export default Preview;
